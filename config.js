857e9715f806395b6441926663f83145297354a2
novaragroupa <novara.group.a@gmail.com>
2026-09-15 16:27:16 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ضع هنا رابط الـ Web App اللي هتاخده بعد نشر Google Apps Script
 // مثال: https://script.google.com/macros/s/AKfycb.../exec
-const APPS_SCRIPT_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwZJYstsoQpr5duvtN-qiml-AdceTQwA_S7Xy75zOccQQsO4gP_L6r7dfR0p7yLC2o3/exec";
 
 const SHOP_NAME = "Connect";
